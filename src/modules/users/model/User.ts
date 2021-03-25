6a65eccf89c4a760edea30101774d13dd57f5377
ricardopraxedes <ricardopraxedes1@gmail.com>
2021-03-25 11:07:34 -0300

    resolução do desafio de solid/arquitetura clean

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,7 +1,21 @@
 import { v4 as uuidV4 } from "uuid";
 
 class User {
-  // Complete aqui
+  id: string;
+  name: string;
+  email: string;
+  created_at: Date;
+  updated_at: Date;
+  admin: boolean;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+    if (!this.admin) {
+      this.admin = false;
+    }
+  }
 }
 
 export { User };
